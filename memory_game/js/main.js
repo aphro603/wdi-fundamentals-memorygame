f4731de635db7066b5a2893fc515dbb6c5266e49
GigKloud <webmaster@GigKlouds-MacBook-Pro.local>
2020-01-12 20:11:23 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "Queen";
+let cardTwo = "King";
+let cardThree = "Queen";
+let cardFour = "King";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardFour);
