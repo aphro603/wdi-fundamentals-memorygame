c9829eec2cca610c41e2eaf6c9f74d952ffb2695
Aphro603 <59742291+aphro603@users.noreply.github.com>
2020-01-14 13:52:14 -0500

    added obejects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,48 @@
 //Card logic
-const cards = ["queen","queen","king","king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamons.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!");
+	alert("You found a match!");
 	} 
 	else console.log("Sorry, not a match!");
+	     alert("Sorry, not a match!");
   }
 }
 
 function flipCard(cardId) {
-console.log("You flipped a " + cards[cardId] + ".");
-cardsInPlay.push(cards[cardId]);
+console.log("You flipped a " + cards[cardId].rank + " of " + cards[cardId].suit + ".");
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
 
+flipCard(0);
 flipCard(3);
-flipCard(2);
 
 
